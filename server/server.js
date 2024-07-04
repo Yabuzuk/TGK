ee2c20ca72d8cb6039b524a28b7665cd795bca6a
Yabuzuk <153407123+Yabuzuk@users.noreply.github.com>
2024-07-04 15:03:05 +0900

    Update server.js

--- server/server.js
+++ server/server.js
@@ -4,18 +4,15 @@ const mongoose = require('mongoose');
 const path = require('path');
 const app = express();
 
-// Используйте абсолютный путь к папке 'public'
 app.use(express.static(path.join(__dirname, 'public')));
 
 mongoose.connect(process.env.MONGODB_URI)
   .then(() => console.log('MongoDB подключен...'))
   .catch(err => console.error('Ошибка подключения к MongoDB:', err));
 
-// Встроенные парсеры для обработки тела запроса
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// Схема и модель для элемента
 const ItemSchema = new mongoose.Schema({
   role: String,
   from: String,
@@ -29,12 +26,10 @@ const ItemSchema = new mongoose.Schema({
 
 const Item = mongoose.model('Item', ItemSchema);
 
-// Маршрут GET для корневой страницы
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-// Маршрут POST для добавления элемента
 app.post('/additem', (req, res) => {
   const newItem = new Item(req.body);
   newItem.save()
@@ -45,12 +40,10 @@ app.post('/additem', (req, res) => {
     });
 });
 
-// Маршрут GET для '/additem'
 app.get('/additem', (req, res) => {
   res.status(200).send('Эта страница предназначена для отправки данных.');
 });
 
-// Маршрут GET для получения всех элементов
 app.get('/api/items', async (req, res) => {
   try {
     const items = await Item.find();
@@ -60,13 +53,11 @@ app.get('/api/items', async (req, res) => {
   }
 });
 
-// Обработка ошибок для всего приложения
 app.use((err, req, res, next) => {
   console.error(err.stack);
   res.status(500).send('Что-то сломалось!');
 });
 
-// Запуск сервера
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
