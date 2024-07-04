079835d986947631b6f81a406e3a4f00a4e3cd1c
Yabuzuk <153407123+Yabuzuk@users.noreply.github.com>
2024-07-04 15:25:01 +0900

    Update script.js

--- server/public/js/script.js
+++ server/public/js/script.js
@@ -78,15 +78,6 @@ document.addEventListener('DOMContentLoaded', function() {
     });
 }
 
-  function copyToClipboard(text) {
-    const textarea = document.createElement('textarea');
-    textarea.value = text;
-    document.body.appendChild(textarea);
-    textarea.select();
-    document.execCommand('copy');
-    document.body.removeChild(textarea);
-  }
-
   function setRole(role, element) {
     const roleInput = document.getElementById('roleInput');
     roleInput.value = role;
