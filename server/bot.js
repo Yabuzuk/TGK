60ad00eecd17ab50a12808ef7a5ccbcebf96d7c6
Yabuzuk <153407123+Yabuzuk@users.noreply.github.com>
2024-07-07 01:10:19 +0900

    Update bot.js

--- server/bot.js
+++ server/bot.js
@@ -1,7 +1,7 @@
 const { Telegraf } = require('telegraf');
 
-module.exports = function(botToken) {
-  const bot = new Telegraf(botToken);
+module.exports = function(6580511339:AAEr-GaoWoHCuh3laUfmD7AqcGqHyomWJno) {
+  const bot = new Telegraf(6580511339:AAEr-GaoWoHCuh3laUfmD7AqcGqHyomWJno);
   bot.start((ctx) => ctx.reply('Welcome!'));
   // Добавьте другие команды бота здесь, если необходимо
   return bot;
