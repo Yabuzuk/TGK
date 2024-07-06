ff0f3c4486ce6c62a1900fe9ce7694473014c640
Yabuzuk <153407123+Yabuzuk@users.noreply.github.com>
2024-07-07 00:05:06 +0900

    Delete bot.js

--- bot.js
+++
@@ -1,8 +0,0 @@
-const { Telegraf } = require('telegraf');
-
-module.exports = function(botToken) {
-  const bot = new Telegraf(botToken);
-  bot.start((ctx) => ctx.reply('Welcome!'));
-  // Добавьте другие команды бота здесь, если необходимо
-  return bot;
-};
