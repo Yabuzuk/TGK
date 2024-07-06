e3041941accb4ea7f8db996fbd1c40b7dcda63b4
Yabuzuk <153407123+Yabuzuk@users.noreply.github.com>
2024-07-06 23:07:36 +0900

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,8 +1,8 @@
 const { Telegraf } = require('telegraf');
 
-// Это ваш токен, полученный от BotFather
-const bot = new Telegraf('6580511339:AAEr-GaoWoHCuh3laUfmD7AqcGqHyomWJno');
-
-bot.start((ctx) => ctx.reply('Welcome!'));
-// Добавьте другие команды бота здесь, если необходимо
-bot.launch();
+module.exports = function(botToken) {
+  const bot = new Telegraf(botToken);
+  bot.start((ctx) => ctx.reply('Welcome!'));
+  // Добавьте другие команды бота здесь, если необходимо
+  return bot;
+};
