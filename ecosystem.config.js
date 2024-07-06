0e86f78ebf4312fc1377662876a8c846bc221048
Yabuzuk <153407123+Yabuzuk@users.noreply.github.com>
2024-07-06 22:36:44 +0900

    Update ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,12 +1,9 @@
 module.exports = {
-  apps: [
-    {
-      name: 'server',
-      script: './server.js',
-      env: {
-        PORT: process.env.PORT || 3000,
-        MONGODB_URI: process.env.MONGODB_URI
-      }
-    }
-  ]
+  apps : [{
+    name: "server",
+    script: "server/server.js"
+  },{
+    name: "bot",
+    script: "bot.js"
+  }]
 };
