75e00f3171b1944a34f7f6116ef0bbfaa38bf539
Yabuzuk <153407123+Yabuzuk@users.noreply.github.com>
2024-07-06 22:59:21 +0900

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,7 +1,8 @@
 const { Telegraf } = require('telegraf');
 
-const bot = new Telegraf(process.env.BOT_TOKEN); // Используйте переменную окружения
+// Это ваш токен, полученный от BotFather
+const bot = new Telegraf('6580511339:AAEr-GaoWoHCuh3laUfmD7AqcGqHyomWJno');
 
 bot.start((ctx) => ctx.reply('Welcome!'));
+// Добавьте другие команды бота здесь, если необходимо
 bot.launch();
-
