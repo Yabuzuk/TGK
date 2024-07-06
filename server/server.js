39f6b6b2551b28c7a7aa89fd2f8916ba5a52d0f3
Yabuzuk <153407123+Yabuzuk@users.noreply.github.com>
2024-07-06 19:56:52 +0900

    Update server.js

--- server/server.js
+++ server/server.js
@@ -5,7 +5,7 @@ const path = require('path');
 const fs = require('fs');
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
-const session = require('express-session');
+
 const app = express();
 
 // Подключение к MongoDB
