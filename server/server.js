43b328de8d634f886ff79994f474ba63f26bd33f
Yabuzuk <153407123+Yabuzuk@users.noreply.github.com>
2024-07-11 09:07:49 +0900

    Update server.js

--- server/server.js
+++ server/server.js
@@ -33,7 +33,7 @@ const Item = mongoose.model('Item', ItemSchema);
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: "http://localhost:3000/auth/google/callback"
+    callbackURL: "https://yabuzuk-tgk-ea4b.twc1.net/auth/google/callback"
   },
   function(accessToken, refreshToken, profile, done) {
     // Здесь вы можете сохранить информацию о пользователе в базе данных
