5035340685b9ce5f0e89ed4b630ec2976f16cade
Yabuzuk <153407123+Yabuzuk@users.noreply.github.com>
2024-07-16 11:23:54 +0900

    Update script.js

--- server/public/js/script.js
+++ server/public/js/script.js
@@ -130,25 +130,29 @@ document.addEventListener('DOMContentLoaded', function() {
   document.getElementById('searchTo').addEventListener('input', filterMessages);
 });
 
-// Функция для создания карусели изображений
+ // Функция для создания карусели изображений
 function createImageCarousel(images) {
   const carouselContainer = document.getElementById('carouselContainer');
   images.forEach((image, index) => {
-        let imgElement = document.createElement('img');
-    imgElement.src = image;
+    let imgElement = document.createElement('img');
+    imgElement.src = `images/${image}`;
     imgElement.classList.add('carousel-image');
-    if (index === 0) {
-      imgElement.classList.add('active');
-    }
+    if (index === 0) imgElement.classList.add('active');
     carouselContainer.appendChild(imgElement);
   });
 
   let currentIndex = 0;
+  const totalImages = images.length;
   setInterval(() => {
     const images = document.querySelectorAll('.carousel-image');
     images[currentIndex].classList.remove('active');
-    currentIndex = (currentIndex + 1) % images.length;
+    currentIndex = (currentIndex + 1) % totalImages;
     images[currentIndex].classList.add('active');
-  }, 3000); // Меняем изображение каждые 3 секунды
+  }, 3000); // Изменение каждые 3 секунды
 }
 
+// Запрос к серверу для получения списка изображений
+fetch('/images')
+  .then(response => response.json())
+  .then(images => createImageCarousel(images))
+  .catch(error => console.error('Ошибка:', error));
